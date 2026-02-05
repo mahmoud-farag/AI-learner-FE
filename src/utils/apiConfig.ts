0747058d708c740e7d3eaf5d7416a38a74d019b9
mahmoud-farag <mahmoud32salamn@gmail.com>
2026-02-05 15:02:59 +0200

    update the baseURL

--- src/utils/apiConfig.ts
+++ src/utils/apiConfig.ts
@@ -4,11 +4,6 @@
 // 'development' | 'production' | 'test'
 const env_type: string = import.meta.env.MODE;
 
-export const BASE_URL: string = env_type === 'production' ? 'will be later'
-  :
-  env_type === 'development' ?
-    'http://localhost:4000'
-    :
-    'will be later';
+export const BASE_URL: string = import.meta.env.VITE_API_URL || 'http://localhost:4000';
 
 
